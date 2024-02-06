376b29a78887852be7aff42ca879f980bf817054
akashsy05 <akashsy05@gmail.com>
2024-02-07 00:53:02 +0530

    Initial commit: Add project files

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+mongoose.set('strictQuery', false);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Connected to Mongodb Database ${mongoose.connection.host}`.bgMagenta
+        .white
+    );
+  } catch (error) {
+    console.log(`MONGO Connect Error ${error}`.bgRed.white);
+  }
+};
+
+module.exports = connectDB;
